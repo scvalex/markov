1fc08b78eb3fe48badb854f3d028830f33815103
Alexandru Scvortov <scvalex@gmail.com>
2013-05-10 19:15:59 +0100

    improve and document markov.c

--- c/markov.c
+++ c/markov.c
@@ -1,69 +1,112 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-int k = 2;
-char inputchars[20000000];
-char *word[4000000];
-int nword;
+const int ngram_n = 3;
 
-int wordncmp(char *p, char *q) {
-    int n = k;
-    for (; *p == *q; ++p, ++q)
-        if ((*p == 0) && (--n == 0))
-            return 0;
+/* Compare the `ngram_n` words. */
+int wordsncmp(char *p, char *q) {
+    int n = ngram_n;
+    for (; *p == *q; ++p, ++q) {
+        if (*p == 0) {
+            n--;
+            if (n == 0) {
+                return 0;
+            }
+        }
+    }
     return (*p-*q);
 }
 
 int sortcmp(const void *a, const void *b) {
-    return wordncmp(*(char**)a, *(char**)b);
+    return wordsncmp(*(char**)a, *(char**)b);
 }
 
-char *skip(char *c, int n) {
-    for (; n > 0; ++c)
-        if (*c == 0)
+/* Move `text` by skipping `n` words. */
+char* skip_words(char *text, int n) {
+    for (; n > 0; ++text)
+        if (*text == 0)
             --n;
-    return c;
+    return text;
 }
 
 int main(int argc, char *argv[]) {
     srand(time(0));
 
-    word[0] = inputchars;
-    while (scanf("%s", word[nword]) != EOF) {
-        word[nword+1] = word[nword] + strlen(word[nword]) + 1;
+    /* Reading more than this number of chars will cause a panic. */
+    int text_size = 4 * 1024 * 1024;
+    char *text = (char*)calloc(text_size, sizeof(char));
+    assert(text);
+
+    /* Reading more than this number of words will cause a panic. */
+    int words_count = 1024 * 1024;
+    char **words = (char**)calloc(words_count, sizeof(char*));
+    assert(words);
+
+    /* Read text and split it into space delimited words. */
+    int nword = 0;
+    words[0] = text;
+    while (scanf("%s", words[nword]) != EOF) {
+        assert(nword < words_count - ngram_n);
+        assert(words[nword] < text + text_size - 20);
+
+        /* Note that we're leaving a 0 after each word. */
+        words[nword+1] = words[nword] + strlen(words[nword]) + 1;
         ++nword;
     }
 
-    int i;
-    for (i = 0; i < k; ++i)
-        word[nword][i] = 0;
-    int aux = rand() % nword;
-    for (i = 0; i < k; ++i)
-        printf("%s ", word[aux+i]);
-    qsort(word, nword, sizeof(word[0]), sortcmp);
+    /* Pre-process words. */
+    qsort(words, nword, sizeof(words[0]), sortcmp);
 
-    char *p, *phrase = word[aux];
-    int wordsleft, l, u, m;
-    for (wordsleft = 300; wordsleft > 0; --wordsleft) {
+    /* Print priming words. */
+    /* Find a fullstop. */
+    int aux = -1;
+    while (aux == -1) {
+        aux = rand() % nword;
+        while (words[aux] && words[aux][strlen(words[aux]) - 1] != '.') {
+            ++aux;
+        }
+    }
+    /* words[aux] is now something ending in a fullstop, so don't
+     * print the first word. */
+    for (int i = 1; i < ngram_n && skip_words(words[aux], i) != 0; ++i)
+        printf("%s ", skip_words(words[aux], i));
+
+    char *phrase = words[aux];
+    for (int wordsleft = 300; wordsleft > 0; --wordsleft) {
+        /* Binary search for last printed word. */
+        int l, u, m;
         l = -1;
         u = nword;
         while (l+1 != u) {
             m = (l+u)/2;
-            if (wordncmp(word[m], phrase) < 0)
+            if (wordsncmp(words[m], phrase) < 0)
                 l = m;
             else
                 u = m;
         }
-        for (i = 0; wordncmp(phrase, word[u+i]) == 0; ++i)
-            if (rand() % (i+1) == 0)
-                p = word[u+i];
-        phrase = skip(p, 1);
-        if (strlen(skip(phrase, k-1)) == 0)
-            break;
-        printf("%s ", skip(phrase, k-1));
+
+        /* Find all word sequences that start with `phrase`. */
+        int start = u;
+        int end = u + 1;
+        for (; wordsncmp(phrase, words[end]) == 0; ++end)
+            ;
+        --end;
+
+        /* Select one of the sequences at random. */
+        char *next_phrase = words[start + rand() % (end - start + 1)];
+
+        phrase = skip_words(next_phrase, 1);
+        if (strlen(skip_words(phrase, ngram_n - 1)) == 0)
+            continue;
+        printf("%s ", skip_words(phrase, ngram_n - 1));
     }
+    printf("\n");
+
+    free(words);
+    free(text);
 
     return 0;
 }
